b17c12c06e8d27525bcf1358e5492d8cadf141db
Josué Ayala <ayalavalencia11@gmail.com>
2022-03-03 13:59:27 -0600

    👌 Modules updates

--- components/TwitterPost.vue
+++ components/TwitterPost.vue
@@ -1,26 +1,26 @@
 <template>
-  <section id="tweet-container" class="tweet-container border py-3 px-4">
+  <section class="tweet-container border py-3 px-4 bg-white rounded-2xl">
     <div class="header flex justify-between">
       <div class="flex items-center">
         <Avatar />
         <div class="ml-2">
-          <p class="text-gray-700 font-bold">{{ data.name }}</p>
-          <p class="text-gray-500 font-normal">@{{ data.username }}</p>
+          <p class="text-gray-700 font-bold">Josué Ayala</p>
+          <p class="text-gray-500 font-normal">@joscode</p>
         </div>
       </div>
-      <div></div>
+      <div>-</div>
     </div>
     <div class="body mt-2">
-      <h1 class="text-xl">{{ data.tweet }}</h1>
+      <h1 class="text-xl">Haz el amor y no la guerra (John Lennon)</h1>
       <div class="time mt-3">
         <p class="text-gray-500 font-normal">{{ data.date }}</p>
       </div>
     </div>
     <div class="footer border-t mt-3 pt-3 flex justify-around">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div>-</div>
+      <div>-</div>
+      <div>-</div>
+      <div>-</div>
     </div>
   </section>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,28 @@
 <template>
-  <div class="grid grid-cols-2">
-    <div class="h-screen overflow-auto p-10 flex items-center">
-      <section class="w-full space-y-4">
-        <Input v-model.trim="data.name" label="Name" block />
-        <Input
-          v-model.trim="data.username"
-          label="Username (Twitter handle)"
-          block
-        />
-        <Input v-model.trim="data.tweet" label="Tweet content" block />
-      </section>
-    </div>
-    <div class="h-screen w-full flex items-center justify-center">
+  <div class="h-screen flex items-center justify-center">
+    <div
+      class="
+        w-[520px]
+        h-[386px]
+        flex
+        items-center
+        justify-center
+        bg-gradient-to-b
+        from-[#A68FFB]
+        to-[#E6BDF7]
+        p-[64px]
+      "
+    >
       <TwitterPost :data="data" title="Twitter component" />
-      <button @click="downloadFile()">Download</button>
     </div>
   </div>
 </template>
 
 <script>
-import TwitterPost from '@/components/TwitterPost'
-import Input from '@/components/Input'
 import dayjs from 'dayjs'
-import html2canvas from 'html2canvas'
-import { base64Downloader } from '@/utils/index'
 
 export default {
   name: 'Index',
-  components: {
-    TwitterPost,
-    Input,
-  },
   data() {
     return {
       data: {
@@ -59,13 +51,6 @@ export default {
       setInterval(() => {
         this.data.date = dayjs().format('h:m A · MMM DD, YYYY')
       }, 60 * 1000)
-    },
-
-    async downloadFile() {
-      const image = await html2canvas(
-        document.querySelector('#tweet-container')
-      )
-      base64Downloader(image.toDataURL(), 'Test filename')
     },
   },
 }
