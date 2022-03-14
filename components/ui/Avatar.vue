157a3e4aa6bdc7c8fd831edac14e58e2280ab1fb
Josué Ayala <ayalavalencia11@gmail.com>
2022-03-13 19:26:06 -0600

    😡 Avatar editable logic

--- components/ui/Avatar.vue
+++ components/ui/Avatar.vue
@@ -2,6 +2,7 @@
   <div
     class="h-12 aspect-square bg-gray-200 rounded-full relative overflow-hidden z-0 group">
     <div
+      v-if="editable"
       class="w-full z-[1] cursor-pointer absolute h-full group-hover:bg-black/50 group-hover:opacity-100 opacity-0 text-white flex items-center justify-center inset-0"
       @click="$emit('edit')">
       <Icon name="pencil" />
@@ -14,6 +15,7 @@
 export default {
   name: 'AvatarComponent',
   props: {
+    editable: { type: Boolean, default: false },
     src: { type: String, default: '' },
   },
 };
